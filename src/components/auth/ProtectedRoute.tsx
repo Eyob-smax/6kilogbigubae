5775b2f47e89c790c76d9604a44a221e738518ab
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-06-30 06:02:28 +0300

    Finishing up auth part

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,9 +1,9 @@
-// src/components/ProtectedRoute.tsx
 import { Navigate } from "react-router-dom";
 import { ReactNode, useEffect, useState } from "react";
 import { api } from "../../api/api";
 import LoadingScreen from "../ui/LoadingScreen";
 import Swal from "sweetalert2";
+
 interface ProtectedRouteProps {
   children: ReactNode;
 }
@@ -11,7 +11,6 @@ interface ProtectedRouteProps {
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const checkAuth = async () => {
@@ -21,12 +20,20 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
           setIsAuthenticated(true);
         } else {
           setIsAuthenticated(false);
+          Swal.fire({
+            icon: "error",
+            title: "Access Denied",
+            text: "You are not authorized.",
+          });
         }
       } catch (error) {
         const { message } = error as { message: string };
-        console.error("Authentication check failed:", message);
         setIsAuthenticated(false);
-        setError(message);
+        Swal.fire({
+          icon: "error",
+          title: "Authentication Error",
+          text: message,
+        });
       } finally {
         setLoading(false);
       }
@@ -35,24 +42,12 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
     checkAuth();
   }, []);
 
-  if (isAuthenticated === null) {
-    return <div>Loading...</div>;
+  if (loading || isAuthenticated === null) {
+    return <LoadingScreen />;
   }
 
   if (!isAuthenticated) {
     return <Navigate to="/admin/login" replace />;
-  }
-
-  if (error) {
-    return Swal.fire({
-      icon: "error",
-      title: "Authentication Error",
-      text: error,
-    });
-  }
-
-  if (loading) {
-    return <LoadingScreen />;
   }
 
   return <>{children}</>;
