7177a157f6911abaae4e91a7ff195256eb29e5ba
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-07-07 08:42:04 +0300

    Adding real backing and updating the proxy server

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: " https://gbi-backend.onrender.com",
+        target: "https://gbi-backend.onrender.com",
         changeOrigin: true,
         secure: false,
       },
