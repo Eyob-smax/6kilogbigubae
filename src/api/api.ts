711189a7f5aa4fccd390a72daed3d7a148162088
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-07-10 05:43:10 +0300

    V 1.0 finished last deployment commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -19,7 +19,7 @@ export interface IResponseObject {
 }
 
 export const api = axios.create({
-  baseURL: "http://localhost:5500/api",
+  baseURL: "https://gbi-backend.onrender.com",
   withCredentials: true,
   timeout: 10000,
   headers: {
