0793f21f856953defbbce6e9ec245638d48f2eef
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-09-25 15:08:35 +0300

    Adding some responsiveness to the admin routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5173,
-    host: true,
     proxy: {
       "/api": {
         target: "https://gbi-backend-h76f.vercel.app",
