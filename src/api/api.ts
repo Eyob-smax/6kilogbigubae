c6b3eefe5acf2130721e985bb354b6396f8abe81
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-09-25 15:41:04 +0300

    Adding some responsiveness to the admin routes

--- src/api/api.ts
+++ src/api/api.ts
@@ -19,7 +19,7 @@ export interface IResponseObject {
 }
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://gbi-backend-h76f.vercel.app/api",
   withCredentials: true,
   timeout: 10000,
   headers: {
