a2f3e06d445e42e0146cfc09f0d32ef81d93aadf
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2026-03-09 01:13:46 +0300

    Fix vhange the local dev into production

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 7173,
     proxy: {
       "/api": {
-        target: "http://localhost:6500",
+        target: "https://gbi-backend-h76f.vercel.app",
         changeOrigin: true,
         secure: false,
       },
