bdffaedad470ad017b5e9563c7caef82c4f3a9bd
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2026-03-09 00:02:27 +0300

    fix(auth): prevent /auth/current from running on login page to avoid premature session check and 401 errors

--- src/customhook/useGetCurrentUser.tsx
+++ src/customhook/useGetCurrentUser.tsx
@@ -1,19 +1,27 @@
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../app/store";
 import { fetchCurrentUser } from "../features/auth/authSlice";
+import { useLocation } from "react-router-dom";
 
 export default function useGetCurrentUser() {
   const dispatch = useDispatch<AppDispatch>();
   const { isAuthenticated, status, loading, hasInitialized } = useSelector(
     (state: RootState) => state.auth,
   );
+  const location = useLocation();
 
   useEffect(() => {
-    if (!hasInitialized && status === "idle") {
+    // Only fetch current user if not on login page
+    if (
+      !hasInitialized &&
+      status === "idle" &&
+      location.pathname !== "/admin/login"
+    ) {
       dispatch(fetchCurrentUser());
     }
-  }, [dispatch, hasInitialized, status]);
+  }, [dispatch, hasInitialized, status, location.pathname]);
 
   return {
     hasInitialized,
