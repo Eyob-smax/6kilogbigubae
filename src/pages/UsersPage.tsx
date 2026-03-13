9142a2672120ee4f19800e92485bc04cf573f498
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2026-03-13 22:09:20 +0300

    chore(search): change the debounce delay from 3 to 1

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -104,7 +104,7 @@ export default function UsersPage() {
       </div>
 
       <div className="mb-3 text-xs text-gray-500">
-        Search applies after 3 seconds, or press Enter to search now.
+        Search applies after 1 seconds, or press Enter to search now.
       </div>
 
       <FilterModal
