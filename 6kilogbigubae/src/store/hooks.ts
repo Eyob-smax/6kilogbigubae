246b61c615e2df2a7539f30c044b109c30c1859e
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2026-05-20 12:02:34 +0000

    delete a depulicated 6kilogbigubae foder

--- 6kilogbigubae/src/store/hooks.ts
+++
@@ -1,4 +0,0 @@
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from '../app/store'; // Adjust path if needed
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
